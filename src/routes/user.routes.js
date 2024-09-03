d372b2d04c5069768f7e42a0e575d356ed4bf652
aCodeAnError <rk8791137@gmail.com>
2024-09-03 15:17:40 +0530

    add register controller and bugfixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,21 @@
 import { Router } from "express" // imported router from Router 
 import { registerUser } from "../controllers/user.controller.js"
+import { upload } from "../middlewares/multer.middlerware.js"
 
 const router = Router() // created a router variabele in router is stored
 
-router.route("/register").post(registerUser) //
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser) //
 
 
 export default router
